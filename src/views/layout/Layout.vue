80b922ab97283783c50a5cd841c4d4acb5457b46
1845552343 <1845552343@qq.com>
2020-07-22 12:54:31 +0800

    左侧菜单滚动条

--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
+  <div class="main">
     <el-container>
       <el-aside width = "210px">
-        <div>
-          <img :src = "logo">
-          <span>数矿云平台</span>
+        <div class="sidebar-title">
+          <img class="sidebar-title-image" :src = "logo">
+          <span>管理系统模板</span>
         </div>
         <sidebar></sidebar>
       </el-aside>
@@ -29,6 +29,8 @@ import {
 } from 'element-ui'
 import Sidebar from './components/Sidebar.vue'
 
+const logo = require('../../assets/logo.png')
+
 Vue.use(Container)
 Vue.use(Header)
 Vue.use(Aside)
@@ -37,7 +39,7 @@ Vue.use(Main)
 export default {
   data() {
     return {
-      logo: 'sd',
+      logo,
     }
   },
   components: {
@@ -50,3 +52,26 @@ export default {
   },
 }
 </script>
+
+<style lang="scss" scoped>
+.main {
+  height: 100%;
+  .el-container {
+    height: 100%;
+    // 左侧菜单头部
+    .sidebar-title {
+      display: flex;
+      height: 60px;
+      width: calc(100% - 1px);
+      color: #FFFFFF;
+      background-color: #304156;
+      align-items: center;
+      &-image {
+        width: 30px;
+        height: 30px;
+        padding: 0 10px 0 20px;
+      }
+    }
+  }
+}
+</style>
--- src/views/layout/components/Sidebar.vue
+++ src/views/layout/components/Sidebar.vue
@@ -1,10 +1,6 @@
 <template>
-  <div>
-    <el-menu
-      background-color = '#304156'
-      text-color = '#ffffff'
-      :unique-opened = true
-      >
+  <el-scrollbar>
+    <el-menu background-color='#304156' text-color='#ffffff' :unique-opened=true>
       <el-menu-item>
         <i class="el-icon-message"></i>
         <span slot="title">人员管理</span>
@@ -16,18 +12,83 @@
         </template>
         <el-menu-item>hhhh</el-menu-item>
       </el-submenu>
+      <el-submenu index='1'>
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span slot="title">导航一</span>
+        </template>
+        <el-menu-item>hhhh</el-menu-item>
+      </el-submenu>
+      <el-submenu index='2'>
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span slot="title">导航一</span>
+        </template>
+        <el-menu-item>hhhh</el-menu-item>
+      </el-submenu>
+      <el-submenu index='3'>
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span slot="title">导航一</span>
+        </template>
+        <el-menu-item>hhhh</el-menu-item>
+      </el-submenu>
+      <el-submenu index='4'>
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span slot="title">导航一</span>
+        </template>
+        <el-menu-item>hhhh</el-menu-item>
+      </el-submenu>
+      <el-submenu index='5'>
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span slot="title">导航一</span>
+        </template>
+        <el-menu-item>hhhh</el-menu-item>
+      </el-submenu>
+      <el-submenu index='6'>
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span slot="title">导航一</span>
+        </template>
+        <el-menu-item>hhhh</el-menu-item>
+      </el-submenu>
+      <el-submenu index='7'>
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span slot="title">导航一</span>
+        </template>
+        <el-menu-item>hhhh</el-menu-item>
+      </el-submenu>
+      <el-submenu index='8'>
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span slot="title">导航一</span>
+        </template>
+        <el-menu-item>hhhh</el-menu-item>
+      </el-submenu>
+      <el-submenu index='9'>
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span slot="title">导航一</span>
+        </template>
+        <el-menu-item>hhhh</el-menu-item>
+      </el-submenu>
     </el-menu>
-  </div>
+  </el-scrollbar>
 </template>
 
 <script>
 import Vue from 'vue'
 import {
+  Scrollbar,
   Menu,
   Submenu,
   MenuItem,
 } from 'element-ui'
 
+Vue.use(Scrollbar)
 Vue.use(Menu)
 Vue.use(Submenu)
 Vue.use(MenuItem)
@@ -51,5 +112,19 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+  .el-scrollbar {
+    height: calc(100% - 60px);
+    .el-scrollbar__view {
+      height: 100%;
+      .el-menu {
+        height: 100%;
+        .el-submenu {
+          .el-menu-item {
+            background-color: #1f2d3d !important;
+          }
+        }
+      }
+    }
+  }
 </style>
